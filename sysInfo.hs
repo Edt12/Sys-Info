729ab877ecb503918da433ab75b4e9c4253d3ba7
Edt12 <104518243+Edt12@users.noreply.github.com>
2024-10-01 22:30:24 +0100

    Got Operating System and Archetecture in process of figuring out how to execute batch commands within haskell to get other things such as cpu,gpu,etc

--- sysInfo.hs
+++ sysInfo.hs
@@ -1,1 +1,21 @@
-print(os::string)
+import System.Info
+import System.Process
+import Data.Text.Internal.Fusion (Stream)
+
+
+getOperatingSystem :: String -> String
+getOperatingSystem x = if os == "mingw32"
+                            then "Windows"
+                        else
+                            "linux"
+getCPU :: String -> IO String
+getCPU x = if x == "mingw32"
+                then readProcess "cmd /c dir" {- Figure out why this isnt working when callCommand is-}
+
+            else
+                readProcess "cmd" ["/c","ls"] ""
+main = do
+    --putStrLn is just a function that takes the parameters and then returns an empty tuple
+    putStrLn ("Operating System: " ++ getOperatingSystem os)
+    putStrLn ("Architecture: " ++ arch)
+    callCommand "cmd /c dir"
